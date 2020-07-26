f94a90dbe8c95a742246da87f75113be7fcc09b3
mregister2 <register2@hotmail.com>
2020-07-26 19:01:26 -0400

    Continue cleaning data, creating new variables.

--- NFLData.R
+++ NFLData.R
@@ -24,9 +24,31 @@ NFLData <- as_tibble(NFLData)
 
 str(NFLData)
 
-#Subset useful variables into new tibble
-NFLData2 <- select(NFLData, TmGmID, VH, Team, Result:PrevPerfATS)
+#Remove neutral site games (London & Mexico City). 40 games. They interfere with travel distance measures
+NFLData2 <- filter(NFLData, VH != "LON" & VH != "MEX")
+NFLData2$VH <- fct_drop(NFLData2$VH)
+levels(NFLData2$VH)
+
+#Remove games that end in a tie. 8 games. Make "Result" a binary categorical variable ("WIN", "LOSS")
+NFLData2 <- filter(NFLData2, Result != "TIE")
+NFLData2$Result <- fct_drop(NFLData2$Result)
+levels(NFLData2$Result)
 
 
-ggplot(data = NFLData2) +
-  geom_point(mapping = aes(x = TravelDist, y = MARGIN))
+#Create new variable: TravelDelta = TravelDist-OppTravelDist. This variable can replace both TravelDist and OppTravelDist in model.
+NFLData2 <- NFLData2 %>% mutate(TravelDelta = TravelDist - OppTravelDist)
+
+#Create new variable: RestDelta = Rest-OppRest. Can replace both Rest and OppRest in model.
+NFLData2 <- NFLData2 %>% mutate(RestDelta = Rest - OppRest)
+
+
+
+
+
+
+
+
+
+#Subset useful variables into new tibble ----
+#NFLData2 <- select(NFLData, TmGmID, VH, Team, Result:PrevPerfATS)
+
